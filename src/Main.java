9be39666332b8e921c2f260d9fe81f53bd816932
DaniDuart <danielduarte8925@gmail.com>
2026-05-14 12:13:09 -0500

    logica de disparos

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,88 @@
+import java.util.Scanner;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("BATALLA NAVAL");
+        String[][] tablero = new String[10][10];
 
-        String[][] tablero = new String[5][5];
 
-        // Llenar tablero
+        System.out.println("----------- --BATALLA NAVAL--------  ");
+
+        System.out.println("""
+                Instrucciones.
+                1.~ = Agua.
+                2.B = Barco.
+                3.X = Impacto.
+                4.O = Disparo fallido.
+                """);
+        System.out.println("Objetivo: encontrar y destruir todos los barcos ");
+
+        System.out.println();
+
+        //*Llenar el tablero *//
         for (int i = 0; i < tablero.length; i++) {
             for (int j = 0; j < tablero[i].length; j++) {
-                tablero[i][j] = "~";
+                tablero[i][j] = " ~ ";
             }
         }
 
-        // Colocar barcos
-        tablero[1][2] = "B";
-        tablero[3][4] = "B";
+        //* Mostrar numeros de las columnas *//
+        System.out.print(" ");
+        for (int j = 0; j < tablero[0].length; j++) {
+            System.out.print(j + "   ");
+        }
+        System.out.println();
 
-        // Mostrar tablero
+
+
+        //*Llenar el tablero *//
         for (int i = 0; i < tablero.length; i++) {
+            for (int j = 0; j < tablero[i].length; j++) {
+                tablero[i][j] = " ~ ";
+            }
+        }
+
+        // Crear los barcos*//
+        tablero[2][3] = "B";
+        tablero[2][4] = "B";
+        tablero[5][6] = "B";
+
+        //*Mostrar *//
+        for (int i = 0; i < tablero.length; i++) {
+            System.out.print(i + " ");
             for (int j = 0; j < tablero[i].length; j++) {
                 System.out.print(tablero[i][j] + " ");
             }
             System.out.println();
+
+
+            // ===== Disparos ===== //
+
+            Scanner teclado = new Scanner(System.in);
+
+            System.out.print("Fila: ");
+            int fila = teclado.nextInt();
+
+            System.out.print("Columna: ");
+            int columna = teclado.nextInt();
+
+
+            // Revisar disparo
+            if(tablero[fila][columna].equals("B")){
+
+                System.out.println("¡Impacto!");
+
+                tablero[fila][columna] = "X";
+
+            }
+            else{
+
+                System.out.println("Disparo fallido");
+
+                tablero[fila][columna] = "O";
+
+            }
         }
     }
 }
