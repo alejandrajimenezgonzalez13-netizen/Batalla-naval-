2d4a33b1c7b44470abc9a64ae4305e05db40e6d9
DaniDuart <danielduarte8925@gmail.com>
2026-05-13 22:27:12 -0500

    logica de disparos

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("BATALLA NAVAL");
 
-        System.out.println(" BATALLA NAVAL");
+        String[][] tablero = new String[5][5];
 
-       String[][]tablero  = new String[5][5];
-
-       //*Llenar el tablero *//
-        for(int i =0; i< tablero.length; i++){
-            for (int j=0; j < tablero[i].length; j++){
+        // Llenar tablero
+        for (int i = 0; i < tablero.length; i++) {
+            for (int j = 0; j < tablero[i].length; j++) {
                 tablero[i][j] = "~";
             }
         }
 
-        //* Mostrar el tablero *//
-        for(int i=0; i< tablero.length; i++){
-            for(int j=0; j<tablero[i].length; j++ ){
-                System.out.print(tablero[i][j] +" ");
+        // Colocar barcos
+        tablero[1][2] = "B";
+        tablero[3][4] = "B";
+
+        // Mostrar tablero
+        for (int i = 0; i < tablero.length; i++) {
+            for (int j = 0; j < tablero[i].length; j++) {
+                System.out.print(tablero[i][j] + " ");
             }
             System.out.println();
         }
-
-
-        //* Colocar barcos *//
-        tablero[1][2]= "B";
-        tablero [3][4] = "B";
-
-        for(int i=0; i< tablero.length; i++){
-            for(int j=0; j< tablero[i].length; i++){
-
-            }
-        }
-
-
-
-
-
-
     }
-
 }
