b38e360842b339947a8532298d4ae30b6531f73d
DaniDuart <danielduarte8925@gmail.com>
2026-05-21 10:01:01 -0500

    Disparos

--- src/Main.java
+++ src/Main.java
@@ -1,87 +1,242 @@
 import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    public static void mostrarMenu() {
+
+        System.out.println("----------- --BATALLA NAVAL--------  ");
+        System.out.println();
+
+        System.out.println("Objetivo: encontrar y destruir todos los barcos ");
+        System.out.println();
+
+        System.out.println("""
+                Menu
+                1.Reglas.
+                2.jugar.
+                3.salir.
+                """);
+    }
+
     public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
 
         String[][] tablero = new String[10][10];
 
+        int opcionm = 0;
 
-        System.out.println("----------- --BATALLA NAVAL--------  ");
-
-        System.out.println("""
-                Instrucciones.
-                1.~ = Agua.
-                2.B = Barco.
-                3.X = Impacto.
-                4.O = Disparo fallido.
-                """);
-        System.out.println("Objetivo: encontrar y destruir todos los barcos ");
-
-        System.out.println();
-
-        //*Llenar el tablero *//
-        for (int i = 0; i < tablero.length; i++) {
-            for (int j = 0; j < tablero[i].length; j++) {
-                tablero[i][j] = " ~ ";
-            }
-        }
-
-        //* Mostrar numeros de las columnas *//
-        System.out.print(" ");
-        for (int j = 0; j < tablero[0].length; j++) {
-            System.out.print(j + "   ");
-        }
-        System.out.println();
+        int posicion = 0;
 
 
+        while (opcionm != 3) {
 
-        //*Llenar el tablero *//
-        for (int i = 0; i < tablero.length; i++) {
-            for (int j = 0; j < tablero[i].length; j++) {
-                tablero[i][j] = " ~ ";
-            }
-        }
+            mostrarMenu();
 
-        // Crear los barcos*//
-        tablero[2][3] = "B";
-        tablero[2][4] = "B";
-        tablero[5][6] = "B";
+            opcionm = teclado.nextInt();
 
-        //*Mostrar *//
-        for (int i = 0; i < tablero.length; i++) {
-            System.out.print(i + " ");
-            for (int j = 0; j < tablero[i].length; j++) {
-                System.out.print(tablero[i][j] + " ");
-            }
-            System.out.println();
+            switch (opcionm) {
+
+                case 1:
+
+                    System.out.println("Reglas");
+
+                    System.out.println("""
+                            Instrucciones.
+                            1.~ = Agua.
+                            2.B = Barco.
+                            3.X = Impacto.
+                            4.O = Disparo fallido.
+                            """);
+
+                    System.out.println("Regresar al menu 1.");
+
+                    opcionm = teclado.nextInt();
+
+                    break;
+
+                case 2:
+
+                    int fila = 0;
+                    int columna = 0;
+
+                    System.out.println("----JUGAR----");
+                    System.out.println();
+
+                    /*Llenar el tablero */
+                    for (int i = 0; i < tablero.length; i++) {
+
+                        for (int j = 0; j < tablero[i].length; j++) {
+
+                            tablero[i][j] = " ~ ";
+
+                        }
+                    }
+
+                    /*Crear los barcos*/
+                    tablero[2][3] = "B";
+                    tablero[2][4] = "B";
+                    tablero[5][6] = "B";
+
+                    /* Mostrar numeros de columnas*/
+                    System.out.print("");
+
+                    for (int j = 0; j < tablero.length; j++) {
+
+                        System.out.print(j + "    ");
+
+                    }
+
+                    System.out.println();
+
+                    /*Mostrar el tablero ocultando barcos */
+                    for (int i = 0; i < tablero.length; i++) {
+
+                        System.out.print(i + "   ");
+
+                        for (int j = 0; j < tablero[i].length; j++) {
+
+                            System.out.print("  ~ ");
+
+                        }
+
+                        System.out.println();
+                    }
+
+                    /*Disparos*/
+                    int barcosDestruidos = 0;
+
+                    int intentos = 5;
 
 
-            // ===== Disparos ===== //
+                    /* While para permitir varios disparos */
+                    while(intentos > 0 && barcosDestruidos < 3){
 
-            Scanner teclado = new Scanner(System.in);
+                        System.out.println("\nIntentos restantes: " + intentos);
 
-            System.out.print("Fila: ");
-            int fila = teclado.nextInt();
+                        System.out.println("Barcos destruidos: " + barcosDestruidos);
 
-            System.out.print("Columna: ");
-            int columna = teclado.nextInt();
+                        /* Pedir coordenadas */
+                        System.out.println("Ingrese la fila");
+
+                        fila = teclado.nextInt();
+
+                        System.out.println("Ingrese la columna");
+
+                        columna = teclado.nextInt();
 
 
-            // Revisar disparo
-            if(tablero[fila][columna].equals("B")){
+                        /* Validar coordenadas */
+                        if(fila < 0 || fila >= 10 || columna < 0 || columna >= 10){
 
-                System.out.println("¡Impacto!");
+                            System.out.println("Posicion invalida");
 
-                tablero[fila][columna] = "X";
+                        }
+                        else{
 
-            }
-            else{
+                            /* Revisar impacto */
+                            if(tablero[fila][columna].equals("B")){
 
-                System.out.println("Disparo fallido");
+                                System.out.println("¡Impacto!");
 
-                tablero[fila][columna] = "O";
+                                tablero[fila][columna] = " X ";
 
+                                barcosDestruidos++;
+
+                            }
+
+                            /* Revisar si ya disparo ahi */
+                            else if(tablero[fila][columna].equals(" X ")
+                                    || tablero[fila][columna].equals(" O ")){
+
+                                System.out.println("Ya disparaste en esta posicion");
+
+                            }
+
+                            /* Disparo fallido */
+                            else{
+
+                                System.out.println("Disparo fallido");
+
+                                tablero[fila][columna] = " O ";
+
+                            }
+
+                            /* Mostrar tablero actualizado */
+                            System.out.println("\nTABLERO ACTUALIZADO:\n");
+
+                            System.out.print("");
+
+                            for (int j = 0; j < tablero.length; j++) {
+
+                                System.out.print(j + "    ");
+
+                            }
+
+                            System.out.println();
+
+                            for (int i = 0; i < tablero.length; i++) {
+
+                                System.out.print(i + "   ");
+
+                                for (int j = 0; j < tablero[i].length; j++) {
+
+                                    /* Ocultar barcos */
+                                    if(tablero[i][j].equals("B")){
+
+                                        System.out.print("  ~ ");
+
+                                    }
+                                    else{
+
+                                        System.out.print(tablero[i][j]);
+
+                                    }
+
+                                }
+
+                                System.out.println();
+                            }
+
+                            /* Restar intentos */
+                            intentos--;
+
+                        }
+
+                    }
+
+
+                    /* Resultado final */
+                    if(barcosDestruidos == 3){
+
+                        System.out.println("\n¡¡GANASTE LA PARTIDA!!");
+
+                    }
+                    else{
+
+                        System.out.println("\nTe quedaste sin intentos");
+
+                    }
+
+                    System.out.println("Regresar al menu 2.");
+
+                    opcionm = teclado.nextInt();
+
+                    break;
+
+                case 3:
+
+                    System.out.println(" Saliendo del juego....");
+
+                    System.out.println(" Gracias por jugar Batalla Naval1");
+
+                    System.out.println("Regresar al menu 3.");
+
+                    opcionm = teclado.nextInt();
+
+                    break;
             }
         }
     }
